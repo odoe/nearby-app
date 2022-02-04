a155788e019282b3d296e8a557a5bd6805d45bb0
Rene Rubalcava <rrubalcava@esri.com>
2022-02-04 15:24:58 -0800

    add routing

---
+++ src/data/routing.spec.ts
@@ -0,0 +1,53 @@
+const mock_getDirections = jest.fn().mockReturnValue({})
+const mock_load = jest.fn()
+const mock_removeAll = jest.fn()
+const mock_addMany = jest.fn()
+const mock_reset = jest.fn()
+const mock_travelMode = {
+    name: 'Walking Time'
+}
+
+import Graphic from '@arcgis/core/Graphic'
+import MapView from '@arcgis/core/views/MapView'
+import DirectionsViewModel from '@arcgis/core/widgets/Directions/DirectionsViewModel'
+import * as routing from './routing'
+
+jest.mock('@arcgis/core/views/MapView')
+
+jest.mock('@arcgis/core/widgets/Directions/DirectionsViewModel', () => {
+    return jest.fn().mockImplementation(() => {
+        return {
+            getDirections: mock_getDirections,
+            load: mock_load,
+            stops: {
+                removeAll: mock_removeAll,
+                addMany: mock_addMany
+            },
+            selectedTravelMode: null,
+            travelModes: {
+                find: jest.fn().mockReturnValue(mock_travelMode)
+            },
+            reset: mock_reset
+        }
+    })
+})
+
+
+describe('data/routing', () => {
+    it('should get directions between two locations', async () => {
+        const start = new Graphic()
+        const stop = new Graphic()
+        const view = new MapView()
+        await routing.getDirections({ start, stop, view })
+        expect(DirectionsViewModel).toHaveBeenCalledTimes(1)
+        expect(mock_load).toHaveBeenCalledTimes(1)
+        expect(mock_removeAll).toHaveBeenCalledTimes(1)
+        expect(mock_addMany).toHaveBeenCalledWith([start, stop])
+        expect(mock_getDirections).toHaveBeenCalledTimes(1)
+    })
+
+    it('should reset the directions view model', () => {
+        routing.clearDirections()
+        expect(mock_reset).toHaveBeenCalledTimes(1)
+    })
+})
---
+++ src/data/routing.ts
@@ -0,0 +1,29 @@
+import Graphic from "@arcgis/core/Graphic";
+import MapView from "@arcgis/core/views/MapView";
+import DirectionsViewModel from "@arcgis/core/widgets/Directions/DirectionsViewModel";
+
+interface GetDirectionsParams {
+    start: Graphic,
+    stop: Graphic,
+    view: MapView
+}
+
+const directionsVM = new DirectionsViewModel()
+
+export async function getDirections({ start, stop, view }: GetDirectionsParams) {
+    directionsVM.view = view
+    await directionsVM.load()
+    directionsVM.stops.removeAll()
+    directionsVM.stops.addMany([start, stop])
+    const walkingTravelMode = directionsVM.travelModes.find(mode => mode.name === 'Walking Time')
+    if (walkingTravelMode) {
+        directionsVM.selectedTravelMode = walkingTravelMode
+    }
+
+    const routeResult = await directionsVM.getDirections()
+    return routeResult
+}
+
+export function clearDirections() {
+    directionsVM.reset()
+}
